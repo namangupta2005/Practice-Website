d1959686522c6131c112438265e1461ca51265a7
namangupta2005 <144221205+namangupta2005@users.noreply.github.com>
2023-10-11 19:20:32 +0530

    food-delivery2.php

--- food-delivery2.php
+++
@@ -1,8 +0,0 @@
-<?php
-$name = $_POST["name"];
-
- echo "<h1>Thank You " .$name." For Giving Me a essential Time My Team is Contect With you below 24 hours.</h1>";
-
- echo "Have a Good Day !";
-
-?>
